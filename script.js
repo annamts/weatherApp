1305da924db99ea3f05f0a28abcde8e45b9efe1a
annamts <annaterradas2@gmail.com>
2018-02-09 18:14:02 +0100

    getting rid of callback hell

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 $(document).ready(function() {
-    /*
-    get location name
-    figure out how many weathers there are {Clear, Clouds, Mist, Rain}
-    icon
-    */
 
     function round(value, decimals) {
         return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
     }
+
     var lat;
     var lon;
     var weatherLink;
@@ -18,61 +14,92 @@ $(document).ready(function() {
     var description;
     var location;
 
-    navigator.geolocation.getCurrentPosition(function(position){
+    
 
+    function getCoords() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            weatherLink = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
+            locationLink = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyAW1Vl4zqI3v60vWnyhHJgPf225yXYdkN4";
+            getWeather();
+        });
+    }
 
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        weatherLink = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
-        locationLink = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyAW1Vl4zqI3v60vWnyhHJgPf225yXYdkN4";
-        
+    function getWeather() {
         $.getJSON(weatherLink, function(json) {
             $("#location").html("");
             temp = json.main.temp;
             units = "C";
+            console.log("1"+units);
             sky = json.weather[0].main;
             description = json.weather[0].description;
+            getLocation();
+            changeHTML();
+        });
+    }
 
-            $.getJSON(locationLink, function(json) {
-                location = json.results[0].address_components[2].long_name;
-                $("#location").html('<p class="appear">' + location + '</p>');
-            });
+    function getLocation() {
+        $.getJSON(locationLink, function(json) {
+            location = json.results[0].address_components[2].long_name;
+            $("#location").html('<p class="appear">' + location + '</p>');
+        });
+    }
+
+    function toFahrenheit(units) {
+        if (units === "C") {
+            temp = temp*(9/5)+32;
+            units = "F";
+            $("#temp").html(round(temp, 1));
+            $("#toggle").html("&degF");
+            console.log("2"+units);
+        } else {
+            temp = (temp-32)*5/9;
+            units = "C";
+            $("#temp").html(round(temp, 1));
+            $("#toggle").html("&degC");
+            console.log("3"+units);
+        }
+    }
+
+    function changeHTML() {
+        $("#temp").html('<p class="appear">' + round(temp, 1) + '</p>');
+        $("#degrees").html('<button id="toggle" class="btn appear">&degC</button>');
+        $("#description").html('<p class="appear">' + description + '</p>');
+        if (sky === "Clear") {
+            $("#icon").html('<div class="icon sunny appear"><div class="sun"><div class="rays"></div></div></div>');
+            $("#content").css("background","#87CEFA");
+        } else if (sky === "Rain") {
+            $("#icon").html('<div class="icon rainy appear"><div class="cloud"></div><div class="rain"></div></div>');
+            $("#content").css("background","gray");
+        } else if (sky === "Snow") {
+            $("#icon").html('<div class="icon flurries appear"><div class="cloud"></div><div class="snow"><div class="flake"></div><div class="flake"></div></div></div>');
+            $("#content").css("background","gray");
+        } else if (sky === "Thunderstorm") {
+            $("#icon").html('<div class="icon thunder-storm appear"><div class="cloud"></div><div class="lightning"><div class="bolt"></div><div class="bolt"></div></div></div>');
+            $("#content").css("background","gray");
+        } else {
+            $("#icon").html('<div class="icon cloudy appear"><div class="cloud"></div><div class="cloud"></div></div>');
+            $("#content").css("background","gray");
+        } 
             
-            $("#temp").html('<p class="appear">' + round(temp, 1) + '</p>');
-            $("#degrees").html('<button id="toggle" class="btn appear">&degC</button>');
-            $("#description").html('<p class="appear">' + description + '</p>');
-            if (sky === "Clear") {
-                $("#icon").html('<div class="icon sunny appear"><div class="sun"><div class="rays"></div></div></div>');
-                $("#content").css("background","#87CEFA");
-            } else if (sky === "Rain") {
-                $("#icon").html('<div class="icon rainy appear"><div class="cloud"></div><div class="rain"></div></div>');
-                $("#content").css("background","gray");
-            } else if (sky === "Snow") {
-                $("#icon").html('<div class="icon flurries appear"><div class="cloud"></div><div class="snow"><div class="flake"></div><div class="flake"></div></div></div>');
-                $("#content").css("background","gray");
-            } else if (sky === "Thunderstorm") {
-                $("#icon").html('<div class="icon thunder-storm appear"><div class="cloud"></div><div class="lightning"><div class="bolt"></div><div class="bolt"></div></div></div>');
-                $("#content").css("background","gray");
+        $("#toggle").on("click", function() {
+            if (units === "C") {
+                temp = temp*(9/5)+32;
+                units = "F";
+                $("#temp").html(round(temp, 1));
+                $("#toggle").html("&degF");
+                console.log("2"+units);
             } else {
-                $("#icon").html('<div class="icon cloudy appear"><div class="cloud"></div><div class="cloud"></div></div>');
-                $("#content").css("background","gray");
-            } 
+                temp = (temp-32)*5/9;
+                units = "C";
+                $("#temp").html(round(temp, 1));
+                $("#toggle").html("&degC");
+                console.log("3"+units);
+            }
+        });
+    }
 
+    getCoords();
 
-            $("#toggle").on("click", function() {
-                if (units === "C") {
-                    temp = temp*(9/5)+32;
-                    units = "F";
-                    $("#temp").html(round(temp, 1));
-                    $("#toggle").html("&degF");
-                } else {
-                    temp = (temp-32)*5/9;
-                    units = "C";
-                    $("#temp").html(round(temp, 1));
-                    $("#toggle").html("&degC");
-                }
-            });
-
-        });    
-    });
 });
